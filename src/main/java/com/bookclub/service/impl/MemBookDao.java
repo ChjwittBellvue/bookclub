8cecd6784c01a20202bc2c50fc7c124cfd132c38
Christopher <chjwitt@my365.bellevue.edu>
2025-03-19 10:26:49 -0500

    Adding week3 instructions up to the last page, checking in to add comments to week2

---
+++ src/main/java/com/bookclub/service/impl/MemBookDao.java
@@ -0,0 +1,79 @@
+package com.bookclub.service.impl;
+
+import com.bookclub.model.Book;
+import com.bookclub.service.dao.BookDao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemBookDao implements BookDao {
+    private List<Book> books = new ArrayList<Book>();
+
+    public MemBookDao() {
+        List<String> hitchhikersGuideAuthors = new ArrayList<>();
+        hitchhikersGuideAuthors.add("Douglas Adams");
+        Book hitchhikersGuide = new Book("1",
+                "Hitchhiker's Guide to the Galaxy",
+                "A space comedy",
+                208,
+                hitchhikersGuideAuthors
+                );
+
+        List<String> reaperManAuthors = new ArrayList<>();
+        reaperManAuthors.add("Terry Pratchett");
+        Book reaperMan = new Book("2",
+                "Reaper Man",
+                "A comedy about death personified",
+                208,
+                reaperManAuthors
+        );
+
+        List<String> goodOmensAuthors = new ArrayList<>();
+        goodOmensAuthors.add("Terry Pratchett");
+        goodOmensAuthors.add("Neil Gaiman");
+        Book goodOmens = new Book("3",
+                "Good Omens",
+                "A comedy about an angel and demon working together",
+                400,
+                goodOmensAuthors
+        );
+
+        List<String> endersGameAuthors = new ArrayList<>();
+        endersGameAuthors.add("Orson Scott Card");
+        Book endersGame = new Book("4",
+                "Ender's Game",
+                "A sci-fi struggle for humanity's survival",
+                256,
+                endersGameAuthors
+        );
+
+        List<String> spellmongerAuthors = new ArrayList<>();
+        spellmongerAuthors.add("Terry Mancour");
+        Book spellmonger = new Book("5",
+                "Spellmonger",
+                "A fantasy tale",
+                624,
+                spellmongerAuthors
+        );
+        books.add(hitchhikersGuide);
+        books.add(reaperMan);
+        books.add(goodOmens);
+        books.add(endersGame);
+        books.add(spellmonger);
+    }
+
+    @Override
+    public List<Book> list() {
+        return this.books;
+    }
+
+    @Override
+    public Book find(String key) {
+        for (Book book : this.books) {
+            if (book.getIsbn().equals(key)) {
+                return book;
+            }
+        }
+        return new Book();
+    }
+}
