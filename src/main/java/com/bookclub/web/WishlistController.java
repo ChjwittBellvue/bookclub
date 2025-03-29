c42b061791ad4c744025bfecd97ca79f4639230a
Christopher <chjwitt@my365.bellevue.edu>
2025-03-29 17:59:24 -0500

    Added week5 changes based on instructions

--- src/main/java/com/bookclub/web/WishlistController.java
+++ src/main/java/com/bookclub/web/WishlistController.java
@@ -5,19 +5,28 @@
  */
 package com.bookclub.web;
 import com.bookclub.model.WishlistItem;
-import com.bookclub.service.impl.MemWishlistDao;
+import com.bookclub.service.impl.MongoWishlistDao;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import com.bookclub.service.dao.WishlistDao;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/wishlist")
 public class WishlistController {
+
+    WishlistDao wishlistDao = new MongoWishlistDao();
+
+    @Autowired
+    private void setWishlistDao(WishlistDao wishlistDao) {
+        this.wishlistDao = wishlistDao;
+    }
 
     /**
      * Moves user to wishlist page
@@ -27,8 +36,7 @@ public class WishlistController {
     @RequestMapping(method = RequestMethod.GET)
     public String showWishlist(Model model)
     {
-        MemWishlistDao memWishlistDao = new MemWishlistDao();
-        List<WishlistItem> wishlist = memWishlistDao.list();
+        List<WishlistItem> wishlist = wishlistDao.list();
 
         model.addAttribute("wishlist", wishlist);
         return "wishlist/list";
@@ -55,12 +63,18 @@ public class WishlistController {
     @RequestMapping(method = RequestMethod.POST)
     public String addWishlistItem(@Valid WishlistItem wishlistItem, BindingResult bindingResult)
     {
+
         System.out.println("mywishlist: " + wishlistItem.toString());
         System.out.println(bindingResult.getAllErrors());
 
         if (bindingResult.hasErrors()) {
             return "wishlist/new";
         }
+
+        wishlistDao.add(wishlistItem);
+
         return "redirect:/wishlist";
     } // end addWishlistItem
+
+
 }
