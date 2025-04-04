90d29841508a271a08cb441d645dd4f77fa14438
Christopher <chjwitt@my365.bellevue.edu>
2025-04-04 08:07:20 -0500

    Adding to checkout other branch

---
+++ src/main/java/com/bookclub/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+/*
+ *  REFERENCES
+ *
+ *  Witt, C. (2025). CIS 530 Intermediate Java Programming. Bellevue University, all rights reserved.
+ */
+package com.bookclub.security;
+
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
---
+++ src/main/java/com/bookclub/security/SecurityController.java
@@ -0,0 +1,9 @@
+/*
+ *  REFERENCES
+ *
+ *  Witt, C. (2025). CIS 530 Intermediate Java Programming. Bellevue University, all rights reserved.
+ */
+package com.bookclub.security;
+
+public class SecurityController {
+}
