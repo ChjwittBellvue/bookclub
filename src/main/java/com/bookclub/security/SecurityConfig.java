ae30a3c5a7e6ceb044d13fd08fa1c028c5d081da
Christopher <chjwitt@my365.bellevue.edu>
2025-04-13 20:58:55 -0500

    Initial check-in, trying new security solutions and downgrading pom dependencies

--- src/main/java/com/bookclub/security/SecurityConfig.java
+++ src/main/java/com/bookclub/security/SecurityConfig.java
@@ -6,5 +6,51 @@
 package com.bookclub.security;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+//@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/public/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(withDefaults())
+//                .httpBasic(withDefaults());
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("admin")
+//                .roles("USER")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 }
